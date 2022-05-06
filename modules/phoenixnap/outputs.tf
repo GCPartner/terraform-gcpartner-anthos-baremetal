1908e5245bea6920956f2d4c36f13e59aa8f8994
Cody Hill <codyhill@google.com>
2022-05-06 14:14:57 +0000

    fixed a few mistakes

--- modules/phoenixnap/outputs.tf
+++ modules/phoenixnap/outputs.tf
@@ -18,6 +18,6 @@ output "worker_node_ips" {
 }
 
 output "vlan_id" {
-  value       = local.vlan_id
+  value       = local.private_network.vlan_id
   description = "The vLan ID used for the private network"
 }
